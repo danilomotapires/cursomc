45583681d63830fbfee8348ce214563ed4173214
danilomotapires <danmota90@gmail.com>
2022-05-19 21:27:19 -0300

    Produto e associacao muitos para muitos

--- src/main/java/com/projeto1/cursomc/CursomcApplication.java
+++ src/main/java/com/projeto1/cursomc/CursomcApplication.java
@@ -1,6 +1,5 @@
 package com.projeto1.cursomc;
 
-import java.lang.reflect.Array;
 import java.util.Arrays;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,13 +8,17 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import com.projeto1.cursomc.domain.Categorias;
+import com.projeto1.cursomc.domain.Produto;
 import com.projeto1.cursomc.repositories.CategoriaRepository;
+import com.projeto1.cursomc.repositories.ProdutoRepository;
 
 @SpringBootApplication
 public class CursomcApplication implements CommandLineRunner{
 	
 	@Autowired
 	private CategoriaRepository repo;
+	@Autowired
+	private ProdutoRepository prodRepo;
 
 	public static void main(String[] args) {
 		SpringApplication.run(CursomcApplication.class, args);
@@ -27,7 +30,19 @@ public class CursomcApplication implements CommandLineRunner{
 		Categorias cat1 = new Categorias(null, "Informática");
 		Categorias cat2 = new Categorias(null, "Escritório");
 		
+		Produto p1 = new Produto(null, "Computador", 2000.00);
+		Produto p2 = new Produto(null, "Impressora", 800.00);
+		Produto p3 = new Produto(null, "Mouse", 80.00);
+		
+		cat1.getProdutos().addAll(Arrays.asList(p1, p2, p3));
+		cat2.getProdutos().addAll(Arrays.asList(p2));
+		
+		p1.getCategorias().addAll(Arrays.asList(cat1));
+		p2.getCategorias().addAll(Arrays.asList(cat1, cat2));
+		p3.getCategorias().addAll(Arrays.asList(cat1));
+		
 		repo.saveAll(Arrays.asList(cat1, cat2));
+		prodRepo.saveAll(Arrays.asList(p1, p2, p3));
 	}
 
 	
--- src/main/java/com/projeto1/cursomc/domain/Produto.java
+++ src/main/java/com/projeto1/cursomc/domain/Produto.java
@@ -9,30 +9,35 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
 import javax.persistence.ManyToMany;
 
-
-
 @Entity
-public class Categorias implements Serializable{
+public class Produto implements Serializable{
 	private static final long serialVersionUID = 1L;
 	
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer id;
 	private String nome;
+	private Double preco;
 	
-	@ManyToMany(mappedBy = "categorias")
-	private List<Produto> produtos = new ArrayList<Produto>();
+	@ManyToMany
+	@JoinTable(name = "PRODUTO_CATEGORIA",
+		joinColumns = @JoinColumn(name = "produto_id"),
+		inverseJoinColumns = @JoinColumn(name = "categoria_id")
+	)
+	private List<Categorias> categorias = new ArrayList<Categorias>();
 	
-	public Categorias() {
-		
+	public Produto() {
 	}
 
-	public Categorias(Integer id, String nome) {
+	public Produto(Integer id, String nome, Double preco) {
 		super();
 		this.id = id;
 		this.nome = nome;
+		this.preco = preco;
 	}
 
 	public Integer getId() {
@@ -51,9 +56,25 @@ public class Categorias implements Serializable{
 		this.nome = nome;
 	}
 
+	public Double getPreco() {
+		return preco;
+	}
+
+	public void setPreco(Double preco) {
+		this.preco = preco;
+	}
+
+	public List<Categorias> getCategorias() {
+		return categorias;
+	}
+
+	public void setCategorias(List<Categorias> categorias) {
+		this.categorias = categorias;
+	}
+
 	@Override
 	public int hashCode() {
-		return Objects.hash(id, nome);
+		return Objects.hash(id);
 	}
 
 	@Override
@@ -64,18 +85,9 @@ public class Categorias implements Serializable{
 			return false;
 		if (getClass() != obj.getClass())
 			return false;
-		Categorias other = (Categorias) obj;
-		return Objects.equals(id, other.id) && Objects.equals(nome, other.nome);
+		Produto other = (Produto) obj;
+		return Objects.equals(id, other.id);
 	}
+	
 
-	public List<Produto> getProdutos() {
-		return produtos;
-	}
-
-	public void setProdutos(List<Produto> produtos) {
-		this.produtos = produtos;
-	}
-	
-	
-	
 }
